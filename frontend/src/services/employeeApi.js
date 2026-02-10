7b3ff7288838bb9e23b20e8228a4930c993bd6dd
Kundan <kundan.atel8899@gmail.com>
2026-02-10 22:53:20 +0530

    Update API base URL for production

--- frontend/src/services/employeeApi.js
+++ frontend/src/services/employeeApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/employees",
+  baseURL: "https://employee-management-system-10j2.onrender.com/api/employees",
 });
 
 export const getEmployees = () => API.get("/");
